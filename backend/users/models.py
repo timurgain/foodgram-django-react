f1431a9fd62b4a29d2cfc090df0d930320ab0fdb
Timur Gain <timur.gain@gmail.com>
2022-06-04 13:15:47 +0300

    user model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,6 +1,32 @@
-from django.db import models
+from collections import namedtuple
+
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+ROLES_NAME = namedtuple('ROLES_NAME', 'user admin')
+ROLES = ROLES_NAME('user', 'admin')
+ROLE_CHOICES = (
+    ('user', ROLES.user),
+    ('admin', ROLES.admin),
+)
 
 
 class User(AbstractUser):
-    pass
+    """Castom model user."""
+    role = models.CharField(
+        verbose_name='Роль пользователя',
+        choices=ROLE_CHOICES,
+        default=ROLES.user,
+        max_length=max(len(role) for _, role in ROLE_CHOICES),
+    )
+
+    class Meta:
+        ordering = ['username']
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def is_admin(self):
+        return self.role == ROLES.admin
+
+    def str(self):
+        return self.username[:30]
