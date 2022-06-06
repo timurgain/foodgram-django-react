2d9572b2affc35d5229b5a71283138ed78dccec2
Timur Gain <timur.gain@gmail.com>
2022-06-07 00:20:12 +0300

    Scaffolding for api views and serializers.

--- backend/api_foodgram/serializers.py
+++ backend/api_foodgram/serializers.py
@@ -1,4 +1,49 @@
+from foodgram.models import (FavoriteRecipes, Ingredient, Recipe, ShoppingCart,
+                             Tag)
+from users.models import User
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
 
-import datetime as dt
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer for Tag model in the foodgram app."""
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'first_name', 'last_name',
+                  'password', 'role',)
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for Tag model in the foodgram app."""
+    class Meta:
+        model = Tag
+        fields = ('name', 'color', 'slug',)
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    """Serializer for Ingredient model in the foodgram app."""
+    class Meta:
+        model = Ingredient
+        fields = ('name', 'measurement_unit')
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serializer for Recipe model in the foodgram app."""
+    class Meta:
+        model = Ingredient
+        fields = ('name', 'tags', 'author', 'ingredients',
+                  'image', 'text', 'cooking_time',)
+
+
+class FavoriteRecipesSerializer(serializers.ModelSerializer):
+    """Serializer for FavoriteRecipes model in the foodgram app."""
+    class Meta:
+        model = Ingredient
+        fields = ('owner', 'recipies')
+
+
+class ShoppingCartSerializer(serializers.ModelSerializer):
+    """Serializer for ShoppingCart model in the foodgram app."""
+    class Meta:
+        model = Ingredient
+        fields = ('owner', 'recipies')
--- backend/api_foodgram/views.py
+++ backend/api_foodgram/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from foodgram.models import (FavoriteRecipes, Ingredient, Recipe, ShoppingCart,
+                             Tag)
+from rest_framework import viewsets
+from users.models import User
 
-# Create your views here.
+from api_foodgram.serializers import (FavoriteRecipesSerializer,
+                                      IngredientSerializer, RecipeSerializer,
+                                      ShoppingCartSerializer, TagSerializer,
+                                      UserSerializer)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """."""
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    """."""
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+
+class IngredientViewSet(viewsets.ModelViewSet):
+    """."""
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """."""
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+
+
+class FavoriteRecipesViewSet(viewsets.ModelViewSet):
+    """."""
+    queryset = FavoriteRecipes.objects.all()
+    serializer_class = FavoriteRecipesSerializer
+
+
+class ShoppingCartViewSet(viewsets.ModelViewSet):
+    """."""
+    queryset = ShoppingCart.objects.all()
+    serializer_class = ShoppingCartSerializer
