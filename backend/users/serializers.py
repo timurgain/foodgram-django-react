2e5d277d69492e3e0099dc679215ef065380825c
Timur Gain <timur.gain@gmail.com>
2022-06-20 23:49:29 +0300

    CustomDjoserUserSerializer.get_is_subscribed

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -17,11 +17,21 @@ class CustomDjoserUserCreateSerializer(UserCreateSerializer):
 class CustomDjoserUserSerializer(UserSerializer):
     """Custom serializer for model User via Djoser."""
 
+    is_subscribed = serializers.SerializerMethodField()
+
     class Meta(UserSerializer.Meta):
         fields = (
             'email', 'id', 'username', 'first_name', 'last_name',
-            # 'is_subscribed',
+            'is_subscribed',
         )
+
+    def get_is_subscribed(self, obj):
+        user = self.context.get('request').user
+        if user.is_anonymous:
+            return False
+        return Follow.objects.filter(user=user, following=obj).exists()
+
+
 
 
 class PostFollowSerializer(serializers.ModelSerializer):
