0a12aaea00bf307178b496ce5acb80e020b30118
Timur Gain <timur.gain@gmail.com>
2022-06-25 16:21:51 +0300

    RecipeFilter is ready.

--- backend/api_foodgram/filters.py
+++ backend/api_foodgram/filters.py
@@ -1,20 +1,34 @@
 import django_filters
 
-from foodgram.models import Recipe, Ingredient, TagInRecipe
+from foodgram.models import Recipe
 
 
 class RecipeFilter(django_filters.FilterSet):
-    """filter for Recipe model and additional fields not from the model."""
-    is_favorited = django_filters.BooleanFilter(method='is_favorited_filter')
-    is_in_shopping_cart = django_filters.BooleanFilter(method='')
-    tags = django_filters.AllValuesMultipleFilter(field_name='tags__slug')
+    """Filter for Recipe model and additional fields not from the model
+    and tags slug-field insted of name-field."""
+    is_favorited = django_filters.BooleanFilter(
+        method='is_favorited_filter',
+        label='is_favorited',
+    )
+    is_in_shopping_cart = django_filters.BooleanFilter(
+        method='is_in_shopping_cart_filter',
+        label='is_in_shopping_cart',
+    )
+    tags = django_filters.AllValuesMultipleFilter(
+        field_name='tags__slug',
+        label='tags',
+    )
 
     class Meta:
         model = Recipe
         fields = ('is_favorited', 'is_in_shopping_cart', 'author', 'tags',)
 
-        def is_favorited_filter(self, queryset, name, value):
-            pass
+    def is_favorited_filter(self, queryset, name, value):
+        if value:
+            return queryset.filter(lovers__user=self.request.user)
+        return queryset
 
-        def is_in_shopping_cart_filter(self, queryset, name, value):
-            pass
+    def is_in_shopping_cart_filter(self, queryset, name, value):
+        if value:
+            return queryset.filter(carts__user=self.request.user)
+        return queryset
