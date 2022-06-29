d7fee630fe72f03bdc03678a34da8f50a0adb5ac
Timur Gain <timur.gain@gmail.com>
2022-06-29 23:00:45 +0300

    STATIC_ROOT

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -158,7 +158,7 @@ if DEBUG:
         os.path.join(BASE_DIR, 'static/'),
     ]
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
