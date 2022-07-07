96aeb978dab946f7854ece2ef1ee22c1b297cbf8
Timur Gain <timur.gain@gmail.com>
2022-07-07 21:15:31 +0300

    DEBUG = True

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', default='some_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = (os.getenv('ALLOWED_HOSTS', default='localhost')).split()
 
